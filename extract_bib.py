d38270e9b703925109d0b7cca0ce3c7b9b369ecc
qub-hri <sadebayo01@qub.ac.uk>
2024-01-06 22:48:06 +0000

    extractt existing reference

--- extract_bib.py
+++ extract_bib.py
@@ -1,11 +1,29 @@
 import os
+import re
 from pathlib import Path
 
 FILE_PATH = 'latex-bib.txt'
 
 with open(FILE_PATH, 'r') as file:
-    file_read = file.readlines()
+    file_read = file.read()
+print(len(file_read))
+pattern_title = r'\\bibitem{\w+}\s*(.+?)\s*["“](.+?)["”].*?(\d{4}).*?(doi:\s*[\d\.\-/]+)?'
 
-pattern = r'\\bibitem{\w+}\s*(.+?)\s*["“].*?(\d{4}).*?(doi:\s*[\d\.\-/]+)?'
 
+matches = re.findall(pattern_title, file_read, re.DOTALL)
 
+extracted_data = []
+for match in matches:
+    author_info, title, year, doi = match
+    author_lastname = author_info.split(',')[0].split()[-1]
+    doi = doi.replace('doi: ', '').strip() if doi else 'N/A'
+
+    extracted_data.append({
+        'author_lastname': author_lastname,
+        'year': year,
+        'title': title,
+        'doi': doi
+    })
+#
+# for j, i in enumerate(extracted_data):
+#     print(j+1, i)
