b536ea63c492f1a0b565e22b04fe94d13b70e78b
qub-hri <sadebayo01@qub.ac.uk>
2024-01-06 22:59:53 +0000

    extractt existing reference

--- extract_bib.py
+++ extract_bib.py
@@ -5,22 +5,27 @@ from pathlib import Path
 FILE_PATH = 'latex-bib.txt'
 
 with open(FILE_PATH, 'r') as file:
-    file_read = file.read()
-print(len(file_read))
-pattern_title = r'\\bibitem{\w+}\s*(.+?)\s*["“](.+?)["”].*?(\d{4}).*?(doi:\s*[\d\.\-/]+)?'
-
-
-matches = re.findall(pattern_title, file_read, re.DOTALL)
+    file_read = file.readlines()
 
 extracted_data = []
-for match in matches:
-    author_info, title, year, doi = match
-    author_lastname = author_info.split(',')[0].split()[-1]
-    doi = doi.replace('doi: ', '').strip() if doi else 'N/A'
+for line in file_read:
+    pattern = r'\\bibitem{\w+}\s*(.*?)[“"](.+?)[”"].*?(\d{4}).*?(doi:\s*[\d\.\-/]+)?'
+    match = re.search(pattern, line, re.DOTALL)
+    if match:
+        author_info, title, year, doi = match.groups()
+        author_lastname = 'unknown'
+        if author_info:
+            possible_lastnames = re.split(r'[,;]', author_info)
+            if possible_lastnames:
+                last_word_in_author = possible_lastnames[0].split()[-1]
+                author_lastname = last_word_in_author if last_word_in_author else 'Unknown'
 
-    extracted_data.append({
-        'author_lastname': author_lastname,
-        'year': year,
-        'title': title,
-        'doi': doi
-    })
+        doi = doi.replace('doi: ', '').strip() if doi else 'N/A'
+        extracted_data.append({
+            'author_lastname': author_lastname,
+            'title': title,
+            'year': year,
+            'doi': doi
+        })
+
+print(len(extracted_data))
